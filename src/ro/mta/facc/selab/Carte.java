3741373e021aa53535ac79a2f870c980331b4316
dan-avram-selab <dan.avram@mta.ro>
2020-11-20 12:22:26 +0200

    Documenteaza Carte si Utilizator
    Issue: #2
    S-au comentat clasele Carte si Utilizator
    
    Signed-off-by: dan-avram-selab <dan.avram@mta.ro>

--- src/ro/mta/facc/selab/Carte.java
+++ src/ro/mta/facc/selab/Carte.java
@@ -1,11 +1,30 @@
 package ro.mta.facc.selab;
 
+
+/**
+ * Modeleaza un obiect de tip carte. Este utilizat in cadrul clasei {@link Biblioteca}
+ * <b>Important:</b> A se utiliza doar in clasa Biblioteca.<br>
+ *
+ * <pre>    Carte carte2 = new Carte("Fratii Karamazov", "Feodor Dostoievski", 1880, 1200);</pre>
+ *
+ * @see Biblioteca
+ * @author dan.avram
+ */
 public class Carte implements Comparable<Carte> {
+    /**
+     * Titlu cartii
+     */
     private String titlu;
     private String autor;
     private int anPublicatie;
     private int nrPagini;
 
+    /**
+     * Se compara cu un alt obiect de tip carte
+     *
+     * @param o Cartea cu care ne compara
+     * @return -1, daca <b>this &#60; o,</b>, 0, daca this == 0 ...
+     */
     @Override
     public int compareTo(Carte o) {
         return Integer.compare(this.nrPagini, o.nrPagini);
